0366ce80a290464d00439588fbf74a9ca7d7483e
Madhvendra3 <rajawatbunty@gmail.com>
2026-07-27 19:46:05 +0100

    Add files via upload

---
+++ list operation.py
@@ -0,0 +1,12 @@
+emptylist = []
+
+num = [1,2,3]
+print(num)
+
+repeat3 = ['a','b','c'] * 2
+print(repeat3)
+
+list = [10,20,30]
+list = list[::-1]
+print(list)
+
---
+++ lists.py
@@ -0,0 +1,7 @@
+mylist= [1,2,'red',10,20,'yellow']
+len(mylist)
+print(mylist[3])
+print(mylist[3:7])
+
+for item in mylist:
+    print(item)
